c1714f2ef870736d0d44a139dcfb336692a1503f
oldboot <55476502+oldboot@users.noreply.github.com>
2021-05-10 11:21:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.19.1/192.168.19.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.19.1/192.168.19.254/g' package/base-files/files/bin/config_generate
